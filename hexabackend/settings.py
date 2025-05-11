90dfbc10245508b35804e730d86549aaff651154
abusaidshabib <abusaidshabib712@gmail.com>
2025-05-11 12:04:19 +0600

    false

--- hexabackend/settings.py
+++ hexabackend/settings.py
@@ -21,26 +21,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://hexadrf.transformsai.com',
 ]
 
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
+CORS_ALLOW_CREDENTIALS = False
 
 
 PROJECT_APPS = ['core', 'users', 'products', 'orders', 'reviews']
